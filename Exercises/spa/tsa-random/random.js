7c12b628117be042401862fbc8a39e8fbee703c9
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-01-26 13:41:30 -0600

    starting

---
+++ Exercises/spa/tsa-random/random.js
@@ -0,0 +1,11 @@
+var wrapper = document.getElementById("wrapper");
+var arrow = document.getElementById("arrow");
+
+function betweenTen() {
+	var number = Math.floor((Math.random() * 10) + 1);
+	return number;
+};
+
+window.addEventListener("click", function() {
+	
+})
