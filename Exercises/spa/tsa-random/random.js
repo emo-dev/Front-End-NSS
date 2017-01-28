fa175858dd971d7a629ee37db7d5f39857273928
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-01-28 12:41:49 -0600

    Finished. Used flexbox for layout.

--- Exercises/spa/tsa-random/random.js
+++ Exercises/spa/tsa-random/random.js
@@ -1,5 +1,5 @@
-var wrapper = document.getElementById("wrapper");
-var arrow = document.getElementById("arrow");
+var wrapper = document.getElementsByClassName("container")[0];
+var arrow = document.getElementsByClassName("centered-div")[0];
 
 function betweenTen() {
 	var number = Math.floor((Math.random() * 10) + 1);
@@ -7,5 +7,10 @@ function betweenTen() {
 };
 
 window.addEventListener("click", function() {
-	
-})
+	var counter = betweenTen();
+	if (counter % 2 === 0) {
+		arrow.style.backgroundImage = "url('images/Left-arrow.png')";
+	} else {
+		arrow.style.backgroundImage = "url('images/Right-arrow.png')";
+	}
+});
