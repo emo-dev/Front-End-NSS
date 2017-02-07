af76de8f4f7bd17abe1e2c9d188073ea4b76c3b5
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-02-07 10:57:39 -0600

    added browserify

--- Exercises/mjd/acme-explosive/lib/Gruntfile.js
+++ Exercises/mjd/acme-explosive/lib/Gruntfile.js
@@ -1,33 +1,40 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+
+    browserify: {
+      '../dist/app.js': ['../js/main.js']
+    },
     jshint: {
       files: ['../js/**/*.js'], //location of javascript files
       options: {
-        predef: ["document", "console", "$", "this", "window", "debugger", "XMLHttpRequest"], //allows for predefined things not found in js
+        predef: ["document", "console", "$", "this", "XMLHttpRequest", "window"], //allows for predefined things not found in js
         esnext: true, //allows for ES6 
         globalstrict: true,
-        globals: {"Acme":true} //name value pairs, allows to define global vars used in many files.
+        globals: {"Acme":true}, //name value pairs, allows to define global vars used in many files.
+        browserify: true
       }
     },
     sass: { //setup sass compilation
       dist: {
         files: {
-          '../css/styles.css': '../sass/styles.scss'
+          '../css/style.css': '../sass/style.scss'
         }
       }
     },
     watch: { //automatically watch for changes
       javascripts: {
         files: ['../js/**/*.js'], 
-        tasks: ['jshint']
+        tasks: ['jshint', 'browserify']
       },
       sass: {
         files: ['../sass/**/*.scss'],
         tasks: ['sass']
-      }
+      },
+      browserify: ['../js/*.js'],
+      tasks: ["browserify"]
     }
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  grunt.registerTask('default', ['jshint', 'watch']);
+  grunt.registerTask('default', ['jshint', 'sass', 'browserify', 'watch']);
 };
