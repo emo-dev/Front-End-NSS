47bf992e122771b14080e35c44482b85c82e56b1
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-02-15 20:58:40 -0600

    Finished Carlot

--- Challenges/spa/carlot/javascripts/Carlot.js
+++ Challenges/spa/carlot/javascripts/Carlot.js
@@ -6,15 +6,16 @@ var CarLot = (function() {
   return {
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
-  	  inventoryLoader.open("GET", "inventory.json");
-  	  inventoryLoader.send();
-
       inventoryLoader.addEventListener("load", function (event) {
-      	console.log("Data Loading is complete.");
+
       	var carData = JSON.parse(event.target.responseText);
       	console.log("Car Data: ", carData);
       	callback(carData);
-      })
+      });
+
+      inventoryLoader.open("GET", "inventory.json");
+      inventoryLoader.send();
+
     }
   };
 
